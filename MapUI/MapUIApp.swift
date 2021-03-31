7832d84efd312dc6b87268b65554a42ad5bf2109
Giorgio Giannotta <jorge_upa@msn.com>
2021-03-31 15:48:18 +0200

    Initial Commit

---
+++ MapUI/MapUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapUIApp.swift
+//  MapUI
+//
+//  Created by Jorge Giannotta on 31/03/21.
+//
+
+import SwiftUI
+
+@main
+struct MapUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
